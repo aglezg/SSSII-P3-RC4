72bf25a9c4317616cd57eafed794d6075ae6eef0
Adrián González Galván <alu0101321219@ull.edu.es>
2022-03-09 20:43:09 +0000

    Función swap y función KSA

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+from ctypes import sizeof
 from operator import mod
 import os
 
@@ -5,16 +6,29 @@ import os
 def cleanTerminal():
   os.system('cls' if os.name == 'nt' else 'clear')
 
+# Intercambiar 2 elementos de un vector
+def swap(vec: list, pos1: int, pos2: int): 
+  if (pos1 < 0 or pos1 >= len(vec)):
+     return None
+  if (pos2 < 0 or pos2 >= len(vec)):
+     return None
+  value1 = vec[pos1]
+  vec[pos1] = vec[pos2]
+  vec[pos2] = value1
+  return vec
+
 # Inicialización (Key Scheduling Algorithm, KSA)
-def KSA(key):
-  S: int = []
+def KSA(key: list):
+  S: list(int) = []
   for i in range(256):
-    S[i] = i
+    S.append(i)
   
   j: int = 0
-  for i in range(255):
-    j: int = mod(j + S[i] + key[i], 256) 
-    # realizar swap
+  for i in range(256):
+    j = mod(j + S[i] + key[i % len(key)], 256) 
+    S = swap(S, i, j)
+
   return S
 
-KSA('a')
+S = KSA([2, 5])
+print(S)
