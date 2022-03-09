acfd056f30b896d4f0ab9d1d8c1151b2b60fa8b5
Adrián González Galván <alu0101321219@ull.edu.es>
2022-03-09 20:12:46 +0000

    Función KSA sin completar

--- functions.py
+++ functions.py
@@ -1,5 +1,20 @@
+from operator import mod
 import os
 
 # Limpia la pantalla de la terminal
 def cleanTerminal():
   os.system('cls' if os.name == 'nt' else 'clear')
+
+# Inicialización (Key Scheduling Algorithm, KSA)
+def KSA(key):
+  S: int = []
+  for i in range(256):
+    S[i] = i
+  
+  j: int = 0
+  for i in range(255):
+    j: int = mod(j + S[i] + key[i], 256) 
+    # realizar swap
+  return S
+
+KSA('a')
