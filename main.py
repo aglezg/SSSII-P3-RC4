030a22c1febb864b80cc9310787688c6fdf3a3fb
Adrián González Galván <alu0101321219@ull.edu.es>
2022-03-09 23:38:02 +0000

    Creamos main

--- main.py
+++ main.py
@@ -1,2 +1,37 @@
 from functions import *
+
+# Main
 cleanTerminal()
+
+# Menú principal  y lectura de opciones
+print("\n PRÁCTICA 3: CIFRADO RC4\n")
+keySeed = input("  Semilla de clave:")
+message = input("  Texto original:")
+    
+# Convertimos a array las entradas para operar con ellas
+keySeedArray = keySeed.replace(' ', '').split(',')
+messageArray = message.replace(' ', '').split(',')
+
+# Las transformamos en arrays de enteros
+intKeySeedArray: list = []
+for element in keySeedArray:
+  if (element.isdigit() and int(element) >= 0):
+    intKeySeedArray.append(int(element))
+  else:
+    print("\n La clave introducida es incorrecta. Finalizando el programa...")
+    quit()
+
+intMessageArray: list = []
+for element in messageArray:
+  if (element.isdigit() and int(element) >= 0):
+    intMessageArray.append(int(element))
+  else:
+    print("\n El mensaje introducido es incorrecto. Finalizando el programa...")
+    quit()
+
+# Cifrado RC4
+cypherMessage = encode(PRGA(KSA(intKeySeedArray), len(intMessageArray)), intMessageArray)
+stringCypherMessage: list = []
+for element in cypherMessage:
+  stringCypherMessage.append(str(element))
+print("\n  > Texto cifrado = "   + ' '.join(stringCypherMessage))
